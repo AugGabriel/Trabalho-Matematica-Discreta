3e6b3911e9872c39a7345443342808c7a7c84bfd
GabrielAugusto <gabriel.galup@gmail.com>
2026-06-28 12:15:54 -0300

    Alteração das datas de criação e modificação de todas as fotos

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 from datetime import datetime
+from win32_setctime import setctime
 
 ROOTDIR = os.path.dirname(os.path.abspath(__file__))
 SIMPATH = os.path.join(ROOTDIR, "simulation/")
@@ -9,7 +10,7 @@ def preorder(directory):
     for entry in directory:
         if not os.path.isdir(entry):
             # Entry is a file
-            date = datetime.fromtimestamp(os.path.getctime(entry)).strftime("%Y/%m")
+            date = datetime.fromtimestamp(os.path.getmtime(entry)).strftime("%Y/%m")
             print(str(entry) + "\t" + date)
         else:
             # Entry is a directory
@@ -19,4 +20,13 @@ def preorder(directory):
 # simulation = [1, 2, 3, [4, 5, [6, 7, 8], 9, 10], 11, [12, 13]]
 simulation = os.scandir(SIMPATH)
 
-preorder(simulation)
+def modify_date():
+    PATH = os.path.join(SIMPATH, "downloads/15-12-2025.avif")
+    DATE = datetime(2025, 12, 15).timestamp()
+    os.utime(PATH, (DATE, DATE))
+    setctime(PATH, DATE)
+
+    print("done")
+
+# preorder(simulation)
+modify_date()
