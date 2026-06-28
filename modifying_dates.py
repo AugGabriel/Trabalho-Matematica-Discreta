b55110c8349c3185a9a97983427f68a24ece15e1
GabrielAugusto <gabriel.galup@gmail.com>
2026-06-28 15:33:47 -0300

    Leve alteração na main, e melhoria e documentação no auxiliar de modifying dates

--- modifying_dates.py
+++ modifying_dates.py
@@ -1,14 +1,22 @@
+"""
+Helper to change both creation and modification dates of every file inside input directory
+"""
+
 from win32_setctime import setctime
 import os
 from datetime import datetime
 
-from main import SIMPATH
+from main import depth_first_search, INPUTDIR
 
 
-def modify_date():
-    PATH = os.path.join(SIMPATH, "downloads/15-12-2025.avif")
-    DATE = datetime(2025, 12, 15).timestamp()
-    os.utime(PATH, (DATE, DATE))
-    setctime(PATH, DATE)
+def modify_date(path: str):
+    day, month, year, *_ = map(int, path.split('\\')[-1].split('.')[0].split('-'))
+    DATE = datetime(year, month, day).timestamp()
+    os.utime(path, (DATE, DATE))
+    setctime(path, DATE)
 
-    print("done")
+
+def modify_dates():
+    [modify_date(file.path) for file in depth_first_search(os.scandir(INPUTDIR))]
+
+modify_dates()
