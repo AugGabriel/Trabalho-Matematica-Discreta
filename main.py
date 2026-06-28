5a8e29c085081a7cc8673e9a8b75895e9bdff6a4
GabrielAugusto <gabriel.galup@gmail.com>
2026-06-28 11:56:02 -0300

    Alterações no preorder, para funcionar corretamente para diretórios e arquivos, mostrando também a data de criação

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
+import os
+from datetime import datetime
+
+ROOTDIR = os.path.dirname(os.path.abspath(__file__))
+SIMPATH = os.path.join(ROOTDIR, "simulation/")
+
+
 def preorder(directory):
-    for elem in directory:
-        if type(elem) == int:
-            print(elem)
+    for entry in directory:
+        if not os.path.isdir(entry):
+            # Entry is a file
+            date = datetime.fromtimestamp(os.path.getctime(entry)).strftime("%Y/%m")
+            print(str(entry) + "\t" + date)
         else:
-            preorder(elem)
+            # Entry is a directory
+            preorder(os.scandir(os.path.abspath(entry)))
 
 
-simulation = [1, 2, 3, [4, 5, [6, 7, 8], 9, 10], 11, [12, 13]]
+# simulation = [1, 2, 3, [4, 5, [6, 7, 8], 9, 10], 11, [12, 13]]
+simulation = os.scandir(SIMPATH)
+
 preorder(simulation)
