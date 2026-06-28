cf04ae270762330dc11317b75d3e659a45406f20
GabrielAugusto <gabriel.galup@gmail.com>
2026-06-28 12:44:50 -0300

    Criação das funções de criar diretório pela data, e criar pela data do arquivo; criação do gitignore; documentação de funções; transformação do preordem em um Generator; e criação da função principal sort

--- main.py
+++ main.py
@@ -1,27 +1,61 @@
 import os
+from os import DirEntry
+import shutil
 from datetime import datetime
+from collections.abc import Generator
 
 ROOTDIR = os.path.dirname(os.path.abspath(__file__))
-INPUTDIR = os.path.join(ROOTDIR, "input/")
-OUTPUTDIR = os.path.join(ROOTDIR, "output/")
+INPUTDIR = os.path.join(ROOTDIR, "input\\")
+OUTPUTDIR = os.path.join(ROOTDIR, "output\\")
 
 
-def preorder(directory):
+def create_dir(date: datetime) -> str:
+    """Creates directory by date, if it still does not exist"""
+
+    # Makes sure the output dir exists
+    if not os.path.exists(OUTPUTDIR):
+        os.mkdir(OUTPUTDIR)
+
+    # Creates the year dir
+    year_path = os.path.join(OUTPUTDIR, f"{date.year:4d}\\")
+    if not os.path.exists(year_path):
+        os.mkdir(year_path)
+
+    # Creates the month dir
+    month_path = os.path.join(year_path, f"{date.month:02d}\\")
+    if not os.path.exists(month_path):
+        os.mkdir(month_path)
+
+    # Returns the path
+    return month_path
+
+
+def create_from_entry(entry: DirEntry):
+    """Creates directory from file's creation date, using create_dir"""
+    return create_dir(datetime.fromtimestamp(os.path.getctime(entry)))
+
+
+def preorder(directory: DirEntry) -> Generator[DirEntry]:
+    """Searches for all photos inside the input dir"""
+
     for entry in directory:
         if not os.path.isdir(entry):
             # Entry is a file
-            date = datetime.fromtimestamp(os.path.getctime(entry)).strftime("%Y/%m")
-            print(str(entry) + "\t" + date)
+            # date = datetime.fromtimestamp(os.path.getctime(entry)).strftime("%Y/%m")
+            yield entry
         else:
             # Entry is a directory
-            preorder(os.scandir(os.path.abspath(entry)))
+            yield from preorder(os.scandir(os.path.abspath(entry)))
 
 
-def create_dir(date: datetime):
-    path = os.path.join()
+def sort():
+    files = preorder(os.scandir(INPUTDIR))
+    for file in files:
+        dst = create_from_entry(file)
+        shutil.copy2(file.path, dst)
 
 
 # simulation = [1, 2, 3, [4, 5, [6, 7, 8], 9, 10], 11, [12, 13]]
-simulation = os.scandir(INPUTDIR)
+# simulation = os.scandir(INPUTDIR)
 
-preorder(simulation)
+sort()
